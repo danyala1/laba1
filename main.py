30293e19def4a7ecf11a92e805464d8f9e8579e9
danyala1 <d.mayasov@mail.ru>
2022-09-26 00:22:10 +0400

    Base(main)

---
+++ main.py
@@ -0,0 +1,10 @@
+import function_work_html_ratio
+def main():
+    for i in range(1,6):
+        os.makedirs("dataset/"+str(i))
+        print('Ищем отзывы для звезды '+str(i))
+        function_work_html_ratio.function_work_html_ratio(i)
+
+
+if __name__=='__main__':
+    main()
