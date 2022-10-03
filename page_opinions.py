8784d9774ad48627e23732019a6e0f80f7816ff2
danyala1 <d.mayasov@mail.ru>
2022-10-04 01:10:15 +0400

    изменил код для нумерации отзывов в файлике

--- page_opinions.py
+++ page_opinions.py
@@ -1,32 +1,21 @@
-import requests
-import info
 from bs4 import BeautifulSoup
-import time
+
+import get_site
 
 
-def page_opinions(j, i, number_opinion,):
-    print("Мы на странице " + str(j) + ' звезды ' + str(i))
-    time.sleep(5)
-    html_text = requests.get(info.base_URL + '/' + str(j) + '/?ratio=' + str(i),
-                             headers={"User-Agent": info.user_agent}).text
-    soup = bs4(html_text, 'lxml')
-    review_title = soup.find_all('a', class_='review-title')
-    review_plus = soup.find_all('a', class_='review-plus')
-    review_minus = soup.find_all('a', class_='review-minus')
-    review_teaser = soup.find_all(class_='review_teaser', itempop='description')
-    for h in range(len(review_title)):
-        text = ''
-        text = review_title[h].text + '\nДостоинства:\n' + plus[h].text + '\n' + 'Недостатки:\n' + minus[
-            h].text + '\n' + '\nОтзыв:\n' + review_teaser[h].text
-        if number_opinion >= 3:  # 1
-            namefile = '0' + str(number_opinion)
-        elif number_opinion >= 2:
-            namefile = '00' + str(number_opinion)
-        elif number_opinion >= 1:
-            namefile = '000' + str(number_opinion)
-        else:
-            namefile = "0000" + str(number_opinion)
-        with open('dataset/' + str(i) + '/' + namefile + '.txt', 'w+') as file:
+def page_opinions(j, i, number_opinion):
+    print(f'Мы на странице {str(j)} звезды {str(i)}')
+    soup = get_site.get_site('/' + str(j) + '/?ratio='+str(i))
+    rewiew_opinion = soup.find_all(class_='review-title')  # находим заголовки(тему)
+    plus = soup.find_all(class_='review-plus')  # находим достоинство
+    minus = soup.find_all(class_='review-minus')  # находим негатив
+    opinion = soup.find_all(class_='review-teaser')  # находим отзыв
+    print(str(len(rewiew_opinion))+str(len(plus))+str(len(minus))+str(len(opinion)))
+    for h in range(20):
+        text = rewiew_opinion[h].text+'\n\nДостоинства: '+plus[h].text+'\nНедостатки: '+minus[h].text+'\nОтзыв: '+opinion[h].text
+        namefile = str(number_opinion).zfill(4)
+        with open('dataset/' + str(i) + '/' + namefile + '.txt', 'w+', encoding="utf-8") as file:
             file.write(text)
-        print('Звезда ' + str(i) + ' отзыв ' + namefile + ' создан')
-        number_opinion += 1
+        print(f'Звезда {str(i)} отзыв {namefile} создан')
+        number_opinion+=1
+    return number_opinion
